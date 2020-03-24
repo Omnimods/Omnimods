2810a2d616df117c29932871504353b50d9cd481
Pezzawinkle <54166225+Pezzawinkle@users.noreply.github.com>
2020-03-24 14:55:35 +1100

    fixed omni-pack icon size

--- omnimatter_science/data.lua
+++ omnimatter_science/data.lua
@@ -3,6 +3,8 @@ require("prototypes.omni-pack")
 if mods["omnimatter_crystal"] then
     data.raw.tool["production-science-pack"].icon = "__omnimatter_science__/graphics/icons/production-science-pack.png"
     table.insert(data.raw["lab"]["lab"].inputs, "omni-pack")
+    data.raw.tool["omni-pack"].icon_size=64
+    data.raw.recipe["omni-pack"].icon_size=64
     if mods["bobtech"] then
         table.insert(data.raw["lab"]["lab-2"].inputs, "omni-pack")
     end
@@ -12,10 +14,4 @@ if mods["omnimatter_crystal"] then
     if data.raw["lab"]["lab-module"] then
         table.insert(data.raw["lab"]["lab-module"].inputs,"omni-pack")
     end
-    -- if data.raw["lab"]["big-lab"] then
-    --     table.insert(data.raw["lab"]["big-lab"].inputs,"omni-pack")
-    -- end
-    -- if data.raw["lab"]["big-lab"] then
-    --     table.insert(data.raw["lab"]["big-lab"].inputs,"ftl-science-pack")
-    -- end
 end
