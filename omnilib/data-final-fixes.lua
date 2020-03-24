e266d6b62d8134847657052dec70fe709e9d2059
Pezzawinkle <54166225+Pezzawinkle@users.noreply.github.com>
2020-03-24 13:49:46 +1100

    removed redundant icon_size override

--- omnilib/data-final-fixes.lua
+++ omnilib/data-final-fixes.lua
@@ -1,11 +1,3 @@
-for _, t in pairs({"recipe","item"}) do
-	for _,d in pairs(data.raw[t]) do
-		if d.icon_size == nil then
-			d.icon_size=32
-		end
-	end
-end
-
 for _,pack in pairs({"automation-science-pack","logistic-science-pack","chemical-science-pack","production-science-pack","military-science-pack","utility-science-pack"}) do
 	data.raw.tool[pack].icon = nil
 	data.raw.tool[pack].icons = {{icon="__omnilib__/graphics/"..pack..".png"}}
