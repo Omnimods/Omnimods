047710e6ee24ab215d0d93f4f83f99bfe1067ba1
SqFKYo <sqfkyo@iki.fi>
2020-03-25 18:37:19 +0200

    Hand sawing trees yields omniwood

--- omnimatter_wood/data-updates.lua
+++ omnimatter_wood/data-updates.lua
@@ -5,17 +5,12 @@ end
 
 if mods["angelsbioprocessing"] then
 	omni.lib.replace_all_ingredient("seedling","omniseedling")
-	
-	local tree_recipes = {"tree-generator-1","tree-generator-2","tree-generator-3",
-						  "desert-tree-generator-1","desert-tree-generator-2","desert-tree-generator-3",
-						  "swamp-tree-generator-1","swamp-tree-generator-2","swamp-tree-generator-3",
-						  "temperate-tree-generator-1","temperate-tree-generator-2","temperate-tree-generator-3"}
-	
-	--data.raw.recipe["wood-sawing-manual"]["normal"].results[1].name="omniwood"
-	--data.raw.recipe["wood-sawing-manual"]["expensive"].results[1].name="omniwood"
-	--data.raw.recipe["wood-sawing-manual"].icons[1].icon = data.raw.item["omniwood"].icons[1].icon
-	--data.raw.recipe["wood-sawing-manual"].icon_size = 32
-	--data.raw.recipe["wood-sawing-manual"].localised_name = {"item-name.omniwood"}
+	omni.lib.replace_recipe_result("wood-sawing-manual","wood","omniwood")
+
+	data.raw.recipe["wood-sawing-manual"].icons[1].icon = data.raw.item["omniwood"].icons[1].icon
+	data.raw.recipe["wood-sawing-manual"].icons[1].icon_size = 32
+	data.raw.recipe["wood-sawing-manual"].icons[1].scale = 1
+	data.raw.recipe["wood-sawing-manual"].localised_name = {"item-name.omniwood"}
 end
 
 if mods["omnimatter_marathon"] then
