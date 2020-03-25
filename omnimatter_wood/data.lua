595fb4680965d48a64caf03b4ae008e32df3625e
SqFKYo <sqfkyo@iki.fi>
2020-03-25 17:04:43 +0200

    Wood sawing gives now omniwood

--- omnimatter_wood/data.lua
+++ omnimatter_wood/data.lua
@@ -15,15 +15,16 @@ if mods["angelsbioprocessing"] then
 	omni.lib.add_recipe_ingredient("algae-red",{type="item",name="omnialgae",amount=40})
 	omni.lib.add_recipe_ingredient("algae-blue",{type="item",name="omnialgae",amount=40})
 	
-	--for i=1,3 do
-	--	local rec = data.raw.recipe["tree-arboretum-"..i]
-	--	omni.marathon.standardise(rec)
-	--	for _,dif in pairs({"normal","expensive"}) do
-	--		rec[dif].results[1].name="omniwood"
+	for i=1,3 do
+		local rec = data.raw.recipe["wood-sawing-"..i]
+		omni.marathon.standardise(rec)
+		for _,dif in pairs({"normal","expensive"}) do
+			rec[dif].results[1].name="omniwood"
 	--		rec[dif].results[1].amount=rec[dif].results[1].amount*4
-	--	end
+		end
 	--	omni.lib.replace_recipe_ingredient("tree-arboretum-"..i,"water","omnic-water")
-	--	rec.icons[1].icon = data.raw.item["omniwood"].icons[1].icon
-	--	rec.localised_name = {"item-name.omniwood"}
-	--end
+		rec.icons[1].icon = data.raw.item["omniwood"].icons[1].icon
+		rec.icon_size = 32
+		rec.localised_name = {"item-name.omniwood"}
+	end
 end
