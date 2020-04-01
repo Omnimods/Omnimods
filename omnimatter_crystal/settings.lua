bb647de3ae0ec110edb0153da0cb7883509d13aa
unknown <samuel.kraft@web.de>
2020-04-01 19:40:28 +0200

    Move Salting Recipes into the Ore Sorting Facility and add "Omni Crystal"

--- omnimatter_crystal/settings.lua
+++ omnimatter_crystal/settings.lua
@@ -2,11 +2,19 @@ data:extend(
 {
   {
     type = "bool-setting",
+    name = "angels-salt-sorting",
+    setting_type = "startup",
+    default_value = true,
+	order=a
+  },
+  {
+    type = "bool-setting",
     name = "omnicrystal-sloth",
     setting_type = "startup",
     default_value = false,
-	order=a
+	order=b
   },
+
 }
 )
 
