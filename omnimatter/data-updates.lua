04aed0deef5b1a6bdd3cbcc3e6c682e32bf1e786
SqFKYo <sqfkyo@iki.fi>
2020-04-05 10:11:03 +0300

    Omniphlogs do not need previous tier buildings to craft

--- omnimatter/data-updates.lua
+++ omnimatter/data-updates.lua
@@ -193,3 +193,14 @@ end
 		}):setOverlay("tractor-over"):
 		extend()
 end
+
+if mods["angelsindustries"] and angelsmods.industries.components then
+	for i=1,settings.startup["omnimatter-max-tier"].value do
+		-- Remove previous tier buildings from the recipes
+		if i == 1 then
+			omni.lib.remove_recipe_ingredient("omniphlog-1", "burner-omniphlog")
+		else
+			omni.lib.remove_recipe_ingredient("omniphlog-"..i, "omniphlog-"..i-1)
+		end
+	end
+end
