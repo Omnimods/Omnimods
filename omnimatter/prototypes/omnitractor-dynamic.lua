7ed03298afe4451605a02b97601ee92caea48813
SqFKYo <sqfkyo@iki.fi>
2020-04-05 20:01:02 +0300

    Burner omnitractor made easier. Early game is a slog as it is

--- omnimatter/prototypes/omnitractor-dynamic.lua
+++ omnimatter/prototypes/omnitractor-dynamic.lua
@@ -1,7 +1,7 @@
 local burner_ingredients = {}
 if mods["angelsindustries"] and angelsmods.industries.components then
 	burner_ingredients = {
-	{name="block-construction-1", amount=3},
+	{name="block-construction-1", amount=1},
 	{name="block-electronics-0", amount=1},
 	{name="block-fluidbox-1", amount=1},
 	{name="block-omni-0", amount=1}}
