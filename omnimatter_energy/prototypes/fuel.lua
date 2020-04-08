2d9b61b5d2f3546224cca363eb48389b855381ba
Pezzawinkle <54166225+Pezzawinkle@users.noreply.github.com>
2020-04-08 23:33:58 +1000

    Localisation fix for Omni-fuels
    used metatable to import data before opening RecGen...

--- omnimatter_energy/prototypes/fuel.lua
+++ omnimatter_energy/prototypes/fuel.lua
@@ -24,68 +24,46 @@ for _,fuelitem in pairs(data.raw.item) do
     --Generate Chemical Fuel Recipes
     if fuelitem.fuel_category == "chemical" and fuelitem.fuel_value and fuelitem.subgroup ~= "omnicell" then
         log("IMPORTING: "..fuelitem.name)
-        --log(serpent.block(fuelitem))
-
-        RecGen:create("omni-energy-"..fuelitem.name):
-            setName("omni-energy-"..fuelitem.name,"omnimatter_energy"):
-            setItemName("omni-energy-"..fuelitem.name):
+        --lets define the variables first, then jump in and create it all in one go:
+        local FV=omni.lib.getFuelNumberInMJ(fuelitem.fuel_value)
+        local props={
+            [5]={ing_add={"crushed-omnite",2},cat="crafting",time=1.0,tech="omnium-power-1"},
+            [10]={ing_add={"pulverized-omnite",4},cat="omnitractor",time=2.0,tech="omnium-power-2"},
+            [40]={ing_add={type = "fluid", name = "omnic-acid", amount = 20},cat="omniphlog",time=2.0,tech="omnium-power-3"},
+            [250]={ing_add={type = "fluid", name = "omnisludge", amount = 80},cat="omniplant",time=4.0,tech="omnium-power-4"},
+            [300]={ing_add={type = "fluid",name = "omniston", amount = 40},cat="omniplant",time=4.0,tech="omnium-power-5"},}
+            local props_add={}
+        if FV<=5 then
+            props_add=props[5]
+        elseif FV<=10 then
+            props_add=props[10]
+        elseif FV<=40 then
+            props_add=props[40]
+        elseif FV<=250 then
+            props_add=props[250]
+        else
+            props_add=props[300]
+        end
+        RecGen:create("omnimatter_energy","omni-energy-"..fuelitem.name):
+            setLocName("omni-energy",fuelitem.localised_name or fuelitem.name):
+            --setName("omni-energy-"..fuelitem.name,"omnimatter_energy"):
+            --setItemName("omni-energy-"..fuelitem.name):
             setIcons(fuelitem.icons or {{icon = fuelitem.icon,icon_size = fuelitem.icon_size}}):
             addSmallIcon("__omnimatter_energy__/graphics/icons/omnicell-charged.png",3):
             setResults({"omni-energy-"..fuelitem.name,1}):
             setSubgroup("omnicell"):
+            setIngredients({fuelitem.name,1},props_add.ing_add):
+            setCategory(props_add.cat):
+            setEnergy(props_add.time):
+            setTechName(props_add.tech):
+            addTechPrereq(omni.lib.find_tech_name(fuelitem.name)):
             setEnabled(false):
+            setStacksize(fuelitem.stack_size):
+            setFuelCategory(fuelitem.fuel_category):
+            setFuelValue(fuelitem.fuel_value):
             extend()
-
-        if omni.lib.getFuelNumberInMJ(fuelitem.fuel_value) <= 5.0 then
-            RecGen:import("omni-energy-"..fuelitem.name,"omnimatter_energy"):
-            setIngredients({fuelitem.name,1},{"crushed-omnite",2}):
-            setCategory("crafting"):
-            setEnergy(1.0):
-            setTechName("omnium-power-1"):
-            --addTechPrereq(omni.lib.find_tech_name(fuelitem.name)):
-            extend() 
-            omni.lib.add_prerequisite("omnium-power-1", omni.lib.find_tech_name(fuelitem.name))  
-        elseif omni.lib.getFuelNumberInMJ(fuelitem.fuel_value) <= 10.0 then
-            RecGen:import("omni-energy-"..fuelitem.name,"omnimatter_energy"):
-            setIngredients({fuelitem.name,1},{"pulverized-omnite",4}):
-            setCategory("omnitractor"):
-            setEnergy(2.0):
-            setTechName("omnium-power-2"):
-           -- addTechPrereq(omni.lib.find_tech_name(fuelitem.name)):
-            extend()
-            omni.lib.add_prerequisite("omnium-power-2", omni.lib.find_tech_name(fuelitem.name))
-        elseif omni.lib.getFuelNumberInMJ(fuelitem.fuel_value) <=40.0 then
-            RecGen:import("omni-energy-"..fuelitem.name,"omnimatter_energy"):
-            setIngredients({fuelitem.name,1},{type = "fluid", name = "omnic-acid", amount = 20}):
-            setCategory("omniphlog"):
-            setEnergy(2.0):
-            setTechName("omnium-power-3"):
-            --addTechPrereq(omni.lib.find_tech_name(fuelitem.name)):
-            extend()
-            omni.lib.add_prerequisite("omnium-power-3", omni.lib.find_tech_name(fuelitem.name))
-        elseif omni.lib.getFuelNumberInMJ(fuelitem.fuel_value) <= 250.0 then
-            RecGen:import("omni-energy-"..fuelitem.name,"omnimatter_energy"):
-            setIngredients({fuelitem.name,1},{type = "fluid", name = "omnisludge", amount = 80}):
-            setCategory("omniplant"):
-            setEnergy(4.0):
-            setTechName("omnium-power-4"):
-            --addTechPrereq(omni.lib.find_tech_name(fuelitem.name)):
-            extend()
-            omni.lib.add_prerequisite("omnium-power-4", omni.lib.find_tech_name(fuelitem.name))
-        else 
-            RecGen:import("omni-energy-"..fuelitem.name,"omnimatter_energy"):
-            setIngredients({fuelitem.name,1},{type = "fluid",name = "omniston", amount = 40}):
-            setCategory("omniplant"):
-            setEnergy(4.0):
-            setTechName("omnium-power-5"):
-            --addTechPrereq(omni.lib.find_tech_name(fuelitem.name)):
-            extend()
-            omni.lib.add_prerequisite("omnium-power-5", omni.lib.find_tech_name(fuelitem.name))
-        end
 
         --Copy over fuel related values
-        data.raw.item["omni-energy-"..fuelitem.name].fuel_category = fuelitem.fuel_category
-        data.raw.item["omni-energy-"..fuelitem.name].fuel_value = fuelitem.fuel_value
         data.raw.item["omni-energy-"..fuelitem.name].fuel_acceleration = fuelitem.fuel_acceleration
         data.raw.item["omni-energy-"..fuelitem.name].fuel_acceleration_multiplier = fuelitem.fuel_acceleration_multiplier
         data.raw.item["omni-energy-"..fuelitem.name].fuel_top_speed = fuelitem.fuel_top_speed
