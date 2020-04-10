923c6f740d5dee3abfa862be5ba4511551d12924
Pezzawinkle <54166225+Pezzawinkle@users.noreply.github.com>
2020-04-10 21:06:38 +1000

    Fixed solar panel icon issue
    Fixed solar panel icon size issue
    Cleaned up the code slightly
    Added localisation for the settings
    Added restrictions for the value for the settings (to not break the function)

--- omnimatter_energy/prototypes/solar_panels.lua
+++ omnimatter_energy/prototypes/solar_panels.lua
@@ -6,7 +6,8 @@ for _, sol in pairs(data.raw["solar-panel"]) do
 end
 
 local sol = {
-	{ 
+	--sol item
+	{
 		type = "item",
 		name = "zolar-panel",
 		icon = "__omnimatter_energy__/graphics/icons/zolar-panel.png",
@@ -16,6 +17,7 @@ local sol = {
 		icon_size = 32,
 		stack_size = 50,
 	},
+	--sol recipe
 	{
 		type = "recipe",
 		name = "zolar-panel",
@@ -26,7 +28,7 @@ local sol = {
 		enabled=false,
 		results=
 		{
-		  {type="item", name="zolar-panel", amount=1},
+			{type="item", name="zolar-panel", amount=1},
 		},
 		order = "a[angelsore1-crushed-hand]",
 	}
@@ -89,7 +91,7 @@ local get_req = function(tier, size, msize)
 		req = {"automation"}
 	elseif size == 1 then
 		req = {"crystal-solar-panel-tier-"..(tier-1).."-size-"..msize}
-	else 
+	else
 		req = {"crystal-solar-panel-tier-"..tier.."-size-"..size-1}
 	end
 	return req
@@ -106,171 +108,141 @@ local get_scienceing = function(tier)
 	if tier <= #techtier then
 		return techtier[tier]
 	else
-		return techtierh[#techtier]
+		return techtier[#techtier]
 	end
-end
-
-local get_icon = function(size)
-	local icons={{icon="__omnimatter_energy__/graphics/icons/empty.png"}}
-	for k=size,1,-1 do
-		for l=1, size do
-			icons[#icons+1]={
-				icon="__omnimatter_energy__/graphics/icons/zolar-panel.png",
-				scale = 1/size,
-			--	shift={((max_size-i)/(max_size-1)+k-2)*32/i,((max_size-i)/(max_size-1)+l-2)*32/i}
-			--	shift={((max_size-i)/(max_size-1)+k-2)*32/i+2-i*2,((max_size-i)/(max_size-1)+l-2)*32/i+2-i*2}
-				shift={((max_size-size)/(max_size-1)+k-2)*32/size-(size-1)*2,((max_size-size)/(max_size-1)+l-2)*32/size-(size-1)*2}
-			}
-		end
-	end
-	for k=1,size-1 do
-		for l=1, size-1 do
-			icons[#icons+1]={
-				icon="__omnimatter_energy__/graphics/icons/zolar-panel.png",
-				scale = 1/size,
-			--	shift={((max_size-i)/(max_size-1)+k-2)*32/i,((max_size-i)/(max_size-1)+l-2)*32/i}
-			--	shift={((max_size-i)/(max_size-1)+k-2)*32/i+2-i*2,((max_size-i)/(max_size-1)+l-2)*32/i+2-i*2}
-				shift={((max_size-size)/(max_size-1)+k-2)*32/size-(size-1)*2,((max_size-size)/(max_size-1)+l-2)*32/size-(size-1)*2}
-			}
-		end
-	end
-	return icons
 end
 
 --log("solar shite")
 for j=1,nr_tiers do
-
-	sol[#sol+1]={ 
+	--subgroup
+	sol[#sol+1]={
 		type = "item-subgroup",
 		name = "omnienergy-solar-tier-"..j,
 		group = "omnienergy",
 		order = data.raw["item-subgroup"]["omnienergy-solar"].order..j,
-	  	}
+	}
 
 	for i=1,max_size do
-	
+		--panel pictures
 		local pic = {}
 		for k=i,1,-1 do
 			for l=1, i do
+				--entity pictures
 				pic[#pic+1]={
-				  filename = "__omnimatter_energy__/graphics/entity/buildings/zolar-panel.png",
-				  priority = "high",
-				  width = 192,
-				  height = 192,
-				  scale=0.5,
-				  shift = {k-i/2-0.5,l-i/2-0.4},
+					filename = "__omnimatter_energy__/graphics/entity/buildings/zolar-panel.png",
+					priority = "high",
+					width = 192,
+					height = 192,
+					scale=0.5,
+					shift = {k-i/2-0.5,l-i/2-0.4},
+				}
+				--icons
+				icons[#icons+1]={
+					icon="__omnimatter_energy__/graphics/icons/zolar-panel.png",
+					icon_size= 32,
+					scale = 1/size,
+					shift={(k-size/2-0.5)*32/size,(l-size/2-0.5)*32/size}
 				}
 			end
 		end
+		--crystal links
 		for k=1,i-1 do
 			for l=1, i-1 do
+				--entity pictures
 				pic[#pic+1]={
-				  filename = "__omnimatter_energy__/graphics/entity/buildings/zolar-crystal.png",
-				  priority = "high",
-				  width = 192,
-				  height = 192,
-				  scale=0.5,
-				  shift = {k-i/2,l-i/2},
+					filename = "__omnimatter_energy__/graphics/entity/buildings/zolar-crystal.png",
+					priority = "high",
+					width = 192,
+					height = 192,
+					scale=0.5,
+					shift = {k-i/2,l-i/2},
+				}
+				--icons
+				icons[#icons+1]={
+					icon="__omnimatter_energy__/graphics/entity/buildings/zolar-crystal.png",
+					icon_size=192,
+					scale = 1/size*72/192,
+					shift={(k-size/2)*32/size,(l-size/2)*32/size}
 				}
 			end
 		end
-		--icons[#icons+1]={icon="__omnimatter_energy__/graphics/icons/zolar-panel.png"}
-		
-		sol[#sol+1]={ 
-		type = "item",
-		name = "crystal-solar-panel-tier-"..j.."-size-"..i,
-		localised_name = {"item-name.crystal-solar-panel", j, i},
-		icons = get_icon(i,32),
-		flags = {},
-		subgroup = "omnitractor",
-		order = "zolar-panel",
-		place_result = "crystal-solar-panel-tier-"..j.."-size-"..i,
-		icon_size = 32,
-		stack_size = 10+max_size*10-10*i,
+		--add tier icon
+		icons[#icons+1]={icon="__omnilib__/graphics/lvl"..tier..".png",icon_size=32} --handles 0-8
+		--solar panel array item sets
+		sol[#sol+1]={
+			type = "item",
+			name = "crystal-solar-panel-tier-"..j.."-size-"..i,
+			localised_name = {"item-name.crystal-solar-panel", j, i},
+			icons = icons,
+			flags = {},
+			subgroup = "omnitractor",
+			order = "zolar-panel",
+			place_result = "crystal-solar-panel-tier-"..j.."-size-"..i,
+			icon_size = 32,
+			stack_size = 10+max_size*10-10*i,
 		}
-		sol[#sol+1]={ 
-		type = "solar-panel",
-		name = "crystal-solar-panel-tier-"..j.."-size-"..i,
-		localised_name = {"entity-name.crystal-solar-panel", j, i},
-		icon = "__base__/graphics/icons/solar-panel.png",
-		icon_size = 32,
-		flags = {"placeable-neutral", "player-creation"},
-		minable = {hardness = 0.2, mining_time = 0.5, result = "crystal-solar-panel-tier-"..j.."-size-"..i},
-		max_health = 200,
-		corpse = "big-remnants",
-		collision_box = {{-i*0.5+0.1, -i*0.5+0.1}, {i*0.5-0.1, i*0.5-0.1}},
-		selection_box = {{-i*0.5, -i*0.5}, {i*0.5, i*0.5}},
-		energy_source =
-		{
-		  type = "electric",
-		  usage_priority = "solar"
-		},
-		picture =
-		{
-		  layers =pic,
-		},
-		--[[overlay =
-		{
-		  layers =
-		  {
+		--solar panel array entity sets
+		sol[#sol+1]={
+			type = "solar-panel",
+			name = "crystal-solar-panel-tier-"..j.."-size-"..i,
+			localised_name = {"entity-name.crystal-solar-panel", j, i},
+			icon = "__base__/graphics/icons/solar-panel.png",
+			icon_size = 32,
+			flags = {"placeable-neutral", "player-creation"},
+			minable = {hardness = 0.2, mining_time = 0.5, result = "crystal-solar-panel-tier-"..j.."-size-"..i},
+			max_health = 200,
+			corpse = "big-remnants",
+			collision_box = {{-i*0.5+0.1, -i*0.5+0.1}, {i*0.5-0.1, i*0.5-0.1}},
+			selection_box = {{-i*0.5, -i*0.5}, {i*0.5, i*0.5}},
+			energy_source =
 			{
-			  filename = "__base__/graphics/entity/solar-panel/solar-panel-shadow-overlay.png",
-			  priority = "high",
-			  width = 108,
-			  height = 90,
-			  shift = util.by_pixel(11, 6),
-			  hr_version = {
-				filename = "__base__/graphics/entity/solar-panel/hr-solar-panel-shadow-overlay.png",
-				priority = "high",
-				width = 214,
-				height = 180,
-				shift = util.by_pixel(10.5, 6),
-				scale = 0.5
-			  }
-			}
-		  }
-		},]]
-		vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
-		production = math.floor(5*math.pow(i,2)*math.pow(1.2,i-1)*math.pow(1.5,j-1)).."kW"
+				type = "electric",
+				usage_priority = "solar"
+			},
+			picture =
+			{
+				layers =pic,
+			},
+			vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
+			production = math.floor(5*math.pow(i,2)*math.pow(1.2,i-1)*math.pow(1.5,j-1)).."kW"
 		}
-
-	  	sol[#sol+1]={
-		type = "recipe",
-		name = "crystal-solar-panel-tier-"..j.."-size-"..i,
-		localised_name = {"recipe-name.crystal-solar-panel", j, i},
-		icons = get_icon(i),
-		icon_size = 32,
-		subgroup = "omnienergy-solar-tier-"..j,
-		category="omniphlog",
-		energy_required = 1,
-		enabled=false,
-		ingredients = get_cost(j,i),
-		results=
-		{
-		  {type="item", name="crystal-solar-panel-tier-"..j.."-size-"..i, amount=1},
-		},
-		energy_required = 6.0,
-		order = "a[angelsore1-crushed-hand]",
+		--solar panel array recipe sets
+		sol[#sol+1]={
+			type = "recipe",
+			name = "crystal-solar-panel-tier-"..j.."-size-"..i,
+			localised_name = {"recipe-name.crystal-solar-panel", j, i},
+			icons = icons,
+			icon_size = 32,
+			subgroup = "omnienergy-solar-tier-"..j,
+			category="omniphlog",
+			energy_required = 1,
+			enabled=false,
+			ingredients = get_cost(j,i),
+			results=
+			{
+				{type="item", name="crystal-solar-panel-tier-"..j.."-size-"..i, amount=1},
+			},
+			energy_required = 6.0,
+			order = "a[angelsore1-crushed-hand]",
 		}
-		
-		sol[#sol+1]={ 
-		type = "technology",
-		name = "crystal-solar-panel-tier-"..j.."-size-"..i,
-		localised_name = {"technology-name.crystal-solar-panel", j, i},
-		--icon = "__omnimatter_energy__/graphics/technology/zolar-panel.png",
-		icons = get_icon(i),
-		icon_size = 32,
-		prerequisites = get_req(j,i,max_size),
-		effects =
-		{
-			{type = "unlock-recipe", recipe = "crystal-solar-panel-tier-"..j.."-size-"..i}
-		},
-		unit =
-		{
-			count = 30+i*30+j*10,
-			ingredients = get_scienceing(j),
-			time = 30
-		},
+		--solar panel array tech unlocks sets
+		sol[#sol+1]={
+			type = "technology",
+			name = "crystal-solar-panel-tier-"..j.."-size-"..i,
+			localised_name = {"technology-name.crystal-solar-panel", j, i},
+			icons = icons,
+			icon_size = 32,
+			prerequisites = get_req(j,i,max_size),
+			effects =
+			{
+				{type = "unlock-recipe", recipe = "crystal-solar-panel-tier-"..j.."-size-"..i}
+			},
+			unit =
+			{
+				count = 30+i*30+j*10,
+				ingredients = get_scienceing(j),
+				time = 30
+			},
 			order = "c-a",
 		}
 	end
