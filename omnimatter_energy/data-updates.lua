dabcb4aa24f3357ce10c5a033ca258b4b6498c94
Cereal Sam <samuel.kraft@web.de>
2020-05-08 18:04:29 +0200

    Add Miniloader support #137

--- omnimatter_energy/data-updates.lua
+++ omnimatter_energy/data-updates.lua
@@ -219,6 +219,12 @@ if data.raw.technology["basic-transport-belt-beltbox"] then
 	extend()
 end
 
+--Miniloader compatibility
+--(The Chute recipe is based on basic belt ingredients (mostly just iron), so we need to add omnitors to that recipe aswell)
+if data.raw.recipe["chute-miniloader"] then
+	omni.lib.add_recipe_ingredient("chute-miniloader",{type = "item", name ="omnitor", amount = 2})
+end
+
 if data.raw.technology["logistics-0"] then
 	-- Finally removing logistics-0
 	omni.lib.remove_prerequisite("logistics","logistics-0")
