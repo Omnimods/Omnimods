ed6f48f8c442f68a7f6260eaabbe960a0d33e21f
Miner239 <siputgila@gmail.com>
2020-05-12 00:08:24 +0700

    fixed omniblocks added without omnimatter

--- omnilib/prototypes/ingredients-generation.lua
+++ omnilib/prototypes/ingredients-generation.lua
@@ -104,7 +104,7 @@ end
 local cmpn = {"vanilla-circuit","crystallonics","vanilla-gear-wheel","bob-gear-wheel","vanilla-gear-box","bob-gear-box","plate","bob-plate","angel-plate","angel-bob-plate","angels-gear-wheel","omni-bob-alloys",
 "omni-plate","vanilla-pipe","bob-pipe","bob-logistics","bob-circuit","omni-alloys","bob-crystallo-circuit","vanilla-crystallo-circuit","angels-comp-circuit","angels-crystallo-circuit","angels-comp-plate"}
 
-if mods["angelsindustries"] and angelsmods.industries.components then
+if mods["angelsindustries"] and mods["omnimatter"] and angelsmods.industries.components then
 	-- DEFINE BLOCKS
 	require("prototypes.omniblocks")
 	component["construction-block"] = {"block-construction-1", "block-construction-2", "block-construction-3", "block-construction-4","block-construction-5"}
