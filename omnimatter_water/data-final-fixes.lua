aa9b342246192d1cfdfc5e66373161ea80ebbf6a
Cereal Sam <Samuel2507@users.noreply.github.com>
2020-07-06 17:22:57 +0200

    Revert "Whoops"
    This reverts commit 8f1b1e21eba5c7841ae72e6a57c9e28c11859560.

--- omnimatter_water/data-final-fixes.lua
+++ omnimatter_water/data-final-fixes.lua
@@ -21,3 +21,18 @@ end
 if mods["aai-industry"] then
 	data.raw["burner-generator"]["burner-turbine"].energy_source.effectivity=0.9/3
 end
+
+--Disable all Pump recipes
+for _,pump in pairs(data.raw["offshore-pump"]) do
+	--Find recipes
+	local rec = omni.lib.find_recipe(pump.name)
+	--Remove recipe from Techs and disable it
+	omni.lib.remove_recipe_all_techs(rec.name)
+
+	if rec.normal or rec.expensive then
+		rec.normal.enabled = false
+		rec.expensive.enabled = false
+	else
+		rec.enabled = false
+	end
+end
