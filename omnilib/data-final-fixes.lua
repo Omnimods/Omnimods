b31cae475b9c02cee339980a72b56a3c486a4c51
oorzkws <65210810+oorzkws@users.noreply.github.com>
2020-07-08 03:54:33 -0600

    Don't override Krastorio2 tech chips
    The models are completely different, we shouldn't be overriding them

--- omnilib/data-final-fixes.lua
+++ omnilib/data-final-fixes.lua
@@ -1,8 +1,10 @@
-for _,pack in pairs({"automation-science-pack","logistic-science-pack","chemical-science-pack","production-science-pack","military-science-pack","utility-science-pack"}) do
-	data.raw.tool[pack].icon = nil
-	data.raw.tool[pack].icons = {{icon="__omnilib__/graphics/icons/science-pack/"..pack..".png",icon_size=64}}
-	if data.raw.technology[pack] then
-		data.raw.technology[pack].icon = "__omnilib__/graphics/technology/"..pack..".png"
+if not mods["Krastorio2"] then
+	for _,pack in pairs({"automation-science-pack","logistic-science-pack","chemical-science-pack","production-science-pack","military-science-pack","utility-science-pack"}) do
+		data.raw.tool[pack].icon = nil
+		data.raw.tool[pack].icons = {{icon="__omnilib__/graphics/icons/science-pack/"..pack..".png",icon_size=64}}
+		if data.raw.technology[pack] then
+			data.raw.technology[pack].icon = "__omnilib__/graphics/technology/"..pack..".png"
+		end
 	end
 end
 
