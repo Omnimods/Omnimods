d58dc32a9c218e91fbb88502cc08d8f80134c465
Cereal Sam <Samuel2507@users.noreply.github.com>
2020-08-22 14:33:14 +0200

    Fix missing check

--- omnimatter_crystal/prototypes/crystallonics.lua
+++ omnimatter_crystal/prototypes/crystallonics.lua
@@ -274,7 +274,7 @@ RecGen:create("omnimatter_crystal","omnine-shards"):
 --[[                         ]]
 
 local crystal_cat = "crystallomnizer"
-if mods["bobmods"] or (angelsmods.industries and angelsmods.industries.overhaul) then
+if mods["bobmods"] or (mods["angelsindustries"] and angelsmods.industries and angelsmods.industries.overhaul) then
   ore_circuit = "lead-ore"
   cry_rod = "tin-ore-crystal"
 else
