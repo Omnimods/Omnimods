d7d49a8358802944acaf9f523e0506b29dd8834d
Pezzawinkle <54166225+Pezzawinkle@users.noreply.github.com>
2020-08-28 01:40:53 +1000

    a

--- omnimatter_energy/prototypes/heresy.lua
+++ omnimatter_energy/prototypes/heresy.lua
@@ -4,8 +4,10 @@
 if mods["pyalienlife"] then
   omni.lib.replace_prerequisite("fluid-handling","automation-2", "anbaricity")
 end
-local rep={
+local replacements={
   {name= "micro-mine-mk01", old= "electric-miner" , new= "burner-miner"},
   {name= "micro-mine-mk01", old= "inserter" , new= "burner-inserter"}
 }
---omni.lib.replace_recipe_ingredient(recipename, ingredient, replacement)
+--[[for i,rep in pairs(replacements) do
+  omni.lib.replace_recipe_ingredient(rep.name, rep.old, rep.new)
+end]]
