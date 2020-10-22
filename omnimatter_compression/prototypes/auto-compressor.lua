bad5f057aca7ca64c128e9c0c3ba718432d74742
Miner239 <siputgila@gmail.com>
2020-10-22 12:46:58 +0700

    allow compressor in space

--- omnimatter_compression/prototypes/auto-compressor.lua
+++ omnimatter_compression/prototypes/auto-compressor.lua
@@ -142,3 +142,7 @@ local b = BuildGen:create("omnimatter_compression","auto-compressor"):
 	
 data.raw["furnace"]["auto-concentrator"].scale_entity_info_icon = true
 data.raw["assembling-machine"]["auto-condensator"].scale_entity_info_icon = true
+
+data.raw["furnace"]["auto-concentrator"].se_allow_in_space = true
+data.raw["furnace"]["auto-compressor"].se_allow_in_space = true
+data.raw["assembling-machine"]["auto-condensator"].se_allow_in_space = true
