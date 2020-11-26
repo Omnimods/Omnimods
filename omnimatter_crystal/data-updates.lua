1e8ec0217c41e9d6d099b237e18c84b84c248c35
Samuel2507 <62474527+Samuel2507@users.noreply.github.com>
2020-11-26 15:11:43 +0100

    Modify crystallogy to output crystal-powder instead of plates to... (#295)
    * Modify crystallogy to output crystal-powder instead of plates to...
    
    be able to use the processing methods of the installed mods (/basegame) together with crystallogy.
    
    This fixes the issue that crystallogy pretty much ignored all secondary outputs that you would  get when processing the specific ore without crystallogy (crystallogy only gave you  u-235 for example).
    
    * Create Powder-versions of all recipes that have crystallogy ores as ingredients
    
    * fix that nested versions of recipes with multiple ores as ingredients were created
    
    * fix missing check that caused a replacement loop for salting recipes + some reordering
    
    * final localisation fixes
    
    * add missing ore tint values
    
    Co-authored-by: Cereal Sam <Samuel2507@users.noreply.github.com>

--- omnimatter_crystal/data-updates.lua
+++ omnimatter_crystal/data-updates.lua
@@ -6,14 +6,15 @@ require("prototypes.recipes.angels")
 --omni.lib.remove_recipe_all_techs("omniplant-1")
 --omni.lib.add_unlock_recipe("omnitech-omnic-acid-hydrolization-1","omniplant-1")
 
+omni.crystal.add_crystal("iron-ore","Iron")
+omni.crystal.add_crystal("copper-ore","Copper")
+omni.crystal.add_crystal("uranium-ore","Uranium")
+
 if mods["Yuoki"] then
 	omni.crystal.add_crystal("y-res1","Durotal")
 	omni.crystal.add_crystal("y-res2","Nuatreel")
 end
 
-omni.crystal.add_crystal("iron-ore","Iron")
-omni.crystal.add_crystal("copper-ore","Copper")
-omni.crystal.add_crystal("uranium-ore","Uranium")
 if mods["omnimatter_marathon"] then
 	omni.marathon.exclude_recipe("omnine-distillation-quick")
 	omni.marathon.exclude_recipe("omnine-distillation-slow")
@@ -47,67 +48,83 @@ if data.raw.item["angels-copper-pebbles-crystal"] then
 	end
 end
 
+--Non Angels case
 if not mods["angelsrefining"] then
-	local turn_to_plate = {}
+	local added_ores = {}
 	for _,rec in pairs(data.raw.recipe) do
 		if string.find(rec.name,"crystal") and omni.lib.end_with(rec.name,"omnitraction") and rec.category=="omnite-extraction" then
 			local ore = rec.normal.results[1].name
-			local metal = ""
-			local plate = ""
-			if ore == "quartz" then
-				plate = "silicon"
-				metal= "silicon-plate"
-			elseif ore == "bauxite-ore" then
-				plate = "aluminium-plate"
-				metal="aluminium"
-			elseif ore == "rutile-ore" then
-				plate = "titanium-plate"
-				metal="titanium"
-			elseif ore == "uranium-ore" then
-				plate = "uranium-235"
-				metal="uranium"
-			elseif ore== "thorium-ore" then
-				plate= "thorium-232"
-				metal="thorium"
-			else
-				plate = string.sub(ore,1,string.len(ore)-string.len("-ore")).."-plate"
-				metal = string.sub(ore,1,string.len(ore)-string.len("-ore"))
-			end
-			if data.raw.item[plate] then
-				rec.normal.results[1].name = plate
-				rec.icon=nil
-				rec.icon_size=nil
-				rec.icons = omni.icon.of(data.raw.item[plate])
-				rec.localised_name = {"recipe-name.item", {"item-name."..plate}}
-				local tier = 1
-				for i,t in pairs(omnisource) do
-					for _,o in pairs(t) do
-						if o.name == ore then
-							tier = o.tier
-						end
+			added_ores[#added_ores+1] = ore
+			local metal = string.gsub(ore,"-ore","")
+
+			local tier = 1
+			for i,t in pairs(omnisource) do
+				for _,o in pairs(t) do
+					if o.name == ore then
+						tier = o.tier
 					end
 				end
-				
-				RecGen:create("omnimatter_crystal", ore.."-salting"):
-					setIngredients({
-						{type="item",name=ore,amount=1},
-						{type="fluid",name="hydromnic-acid",amount=120}}):
-					setResults(ore.."-omnide-salt"):
-					setEnabled(false):
-					setTechName("omnitech-crystallology-"..tier):
-					setIcons(salt_omnide_icon(ore)):
-					setLocName({"recipe-name.omnide-salting", {"item-name."..ore}}):
-					setLocDesc({"recipe-description.pure_extraction", {"item-name."..ore}}):
-					setSubgroup("salting"):
-					setOrder("a[omnide-salting]"..ore):
-					setStacksize(200):
-					setEnergy(5):
-					setCategory("omniplant"):
-					extend()
+			end
+			
+			--Create salting recipes
+			RecGen:create("omnimatter_crystal", ore.."-salting"):
+				setIngredients({
+					{type="item",name=ore,amount=1},
+					{type="fluid",name="hydromnic-acid",amount=120}}):
+				setResults(ore.."-omnide-salt"):
+				setEnabled(false):
+				setTechName("omnitech-crystallology-"..tier):
+				setIcons(salt_omnide_icon(ore)):
+				setLocName({"recipe-name.omnide-salting", {"item-name."..ore}}):
+				setLocDesc({"recipe-description.pure_extraction", {"item-name."..ore}}):
+				setSubgroup("salting"):
+				setOrder("a[omnide-salting]"..ore):
+				setStacksize(200):
+				setEnergy(5):
+				setCategory("omniplant"):
+				extend()
 
-				omni.lib.add_unlock_recipe("omnitech-crystallology-"..tier, ore.."-omnide-solution")
-				omni.lib.add_unlock_recipe("omnitech-crystallology-"..tier, ore.."-crystal-omnitraction")
-				omni.lib.add_unlock_recipe("omnitech-crystallology-"..tier, ore.."-crystal")
+			omni.lib.add_unlock_recipe("omnitech-crystallology-"..tier, ore.."-omnide-solution")
+			omni.lib.add_unlock_recipe("omnitech-crystallology-"..tier, ore.."-crystal-omnitraction")
+			omni.lib.add_unlock_recipe("omnitech-crystallology-"..tier, ore.."-crystal")
+
+			--Create crystal powder item if it doesnt exist yet
+			if not data.raw.item["crystal-powder-"..metal] then
+				ItemGen:create("omnimatter_crystal", "crystal-powder-"..metal):
+					setLocName({"item-name.crystal-powder", {metal}}):
+					setIcons({{
+						icon = "__omnimatter_crystal__/graphics/icons/crystal-powder.png",
+						icon_size = 32,
+						tint = omni.lib.ore_tints[metal] or {r = 1, g = 1, b = 1, a = 1}
+						}}):
+					extend()	
+			end
+
+			--Replace the ore with crystal powder
+			rec.normal.results[1].name = "crystal-powder-"..metal
+			rec.icon=nil
+			rec.icon_size=nil
+			rec.icons = omni.icon.of(data.raw.item["crystal-powder-"..metal])
+			rec.localised_name = {"recipe-name.crystal-powder", {metal}}
+		end
+	end
+
+	for _,rec in pairs(data.raw.recipe) do
+		for _,ore in pairs(added_ores) do
+			--Copy all smelting / processing recipes, make a copy and replace the ore ingredient with crystal-powder (exclude salting recipes!!!)
+			if omni.lib.recipe_ingredient_contains(rec.name, ore) and rec.subgroup~="salting" then --and (string.find(rec.name, "plate") or string.find(rec.name, "processing") ) then
+				local metal = string.gsub(ore,"-ore","")
+
+				--Check if its already a crystal-powder recipe (recipes with multiple ores as ingredients) to avoid the creation of nested powder recipes
+				local r= RecGen:import(rec)
+				if not string.find(rec.name, "crystal%-powder") then
+					r:setName("crystal-powder-"..rec.name)
+				end
+				r:replaceIngredients(ore, "crystal-powder-"..metal):
+				setEnabled(false):
+				setTechName(omni.lib.get_tech_name(ore.."-crystal")):
+				setLocName({"recipe-name.crystalline", omni.locale.of(rec).name }):
+				extend()
 			end
 		end
 	end
