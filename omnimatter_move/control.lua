55f5c1f5c3bfbb20f47e3531ee408db1f1b90005
Cereal Sam <Samuel2507@users.noreply.github.com>
2020-11-27 15:26:09 +0100

    whoops, left commented code in

--- omnimatter_move/control.lua
+++ omnimatter_move/control.lua
@@ -18,12 +18,10 @@ local round = function(nr)
 end
 
 script.on_event(defines.events.on_cutscene_cancelled, function(event)
-	local ply = game.players
-	log("test")
-	log(serpent.block(ply))
-	--if game.shortcut_prototypes["ore-move-planner-shortcut"] then
-	--	ply.set_shortcut_available("ore-move-planner-shortcut", not not ply.force.technologies["rocket-silo"].researched)
-	--end
+	local ply = game.players[event.player_index]
+	if game.shortcut_prototypes["ore-move-planner-shortcut"] then
+		ply.set_shortcut_available("ore-move-planner-shortcut", not not ply.force.technologies["rocket-silo"].researched)
+	end
 end)
 
 
