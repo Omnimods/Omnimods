3665b8a6787c1589c596edc0236064e2c476e0c9
Cereal Sam <Cereal-Sam@users.noreply.github.com>
2020-12-21 16:46:43 +0100

    use factorios custom table_size function (better performance)

--- omnimatter/data-final-fixes.lua
+++ omnimatter/data-final-fixes.lua
@@ -14,14 +14,9 @@ require("prototypes.generation.autoplace-removal")
 --Add last extraction techs as rocket silo prereq
 if omni.rocket_locked then
 	--Get highest ore and fluid tier
-	local max_tier = 0
-	for _,tier in pairs(omni.omnisource) do
-		max_tier = max_tier + 1
-	end
-	local max_fluid_tier = 0
-	for _,tier in pairs(omni.omnifluid) do
-		max_fluid_tier = max_fluid_tier + 1
-	end
+	local max_tier = table_size(omni.omnisource) or 0
+	local max_fluid_tier = table_size(omni.omnifluid) or 0
+
 	--Check if there is a higher fluid than ore tier
 	if max_tier > max_fluid_tier then
 		local pure_extractions = 3 * omni.pure_levels_per_tier
