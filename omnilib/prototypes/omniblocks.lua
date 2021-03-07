19bccd7a53a65e2285c9abc485120ab0c1fe469e
Cereal Sam <62474527+Cereal-Sam@users.noreply.github.com>
2021-03-07 14:39:06 +0100

    lib locale cleanup

--- omnilib/prototypes/omniblocks.lua
+++ omnilib/prototypes/omniblocks.lua
@@ -8,6 +8,7 @@ if mods["angelsindustries"] and mods["omnimatter"] and angelsmods.industries.com
 	{
 		type = "item",
 		name = "block-omni-0",
+		localised_name = {"item-name.block-omni",{0}},
 		icons = {
 				{icon = "__angelsindustries__/graphics/icons/block-bprocessing-4.png",
 				tint = {255,0,255}},
@@ -21,6 +22,7 @@ if mods["angelsindustries"] and mods["omnimatter"] and angelsmods.industries.com
 	{
 		type = "recipe",
 		name = "block-omni-0",
+		localised_name = {"item-name.block-omni",{0}},
 		enabled = true,
 		category = "crafting",
 		energy_required = 5,
@@ -45,6 +47,7 @@ if mods["angelsindustries"] and mods["omnimatter"] and angelsmods.industries.com
 			{
 				type = "item",
 				name = "block-omni-"..i,
+				localised_name = {"item-name.block-omni",{i}},
 				icons = {
 					{icon = "__angelsindustries__/graphics/icons/block-bprocessing-4.png",
 					tint = {255,0,255}},
@@ -58,6 +61,7 @@ if mods["angelsindustries"] and mods["omnimatter"] and angelsmods.industries.com
 			{
 				type = "recipe",
 				name = "block-omni-"..i,
+				localised_name = {"item-name.block-omni",{i}},
 				enabled = false,
 				category = "crafting",
 				energy_required = 5,
