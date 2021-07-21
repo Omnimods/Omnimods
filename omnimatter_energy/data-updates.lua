1bf819fa3b40bcdc2130466f16095773c0ce0a9e
Cereal Sam <62474527+Cereal-Sam@users.noreply.github.com>
2021-07-21 12:07:54 +0200

    no logi sp for miniloader tech anymore

--- omnimatter_energy/data-updates.lua
+++ omnimatter_energy/data-updates.lua
@@ -12,4 +12,4 @@ require("prototypes.compat.angels")
 require("prototypes.compat.angelsindustries")
 require("prototypes.compat.miniloaders")
 require("prototypes.compat.py")
---require("prototypes.compat.krastorio2") TODO
+--require("prototypes.compat.krastorio2") --K2 has to fix things on their side first
