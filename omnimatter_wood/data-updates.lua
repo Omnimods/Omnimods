26a3b1874a6c8a678d785654699497c1c79b920e
Cereal Sam <62474527+Cereal-Sam@users.noreply.github.com>
2021-07-28 18:42:07 +0200

    finally fix algae recipe hiding and some other stuff

--- omnimatter_wood/data-updates.lua
+++ omnimatter_wood/data-updates.lua
@@ -1,24 +1,24 @@
 
 if mods["aai-industry-sp0"] then
-	industry.add_tech("omniwaste")
+    industry.add_tech("omniwaste")
 end
 
 if mods["omnimatter_marathon"] then
-	omni.marathon.equalize("burner-omniphlog","omni-mutator")
+    omni.marathon.equalize("burner-omniphlog","omni-mutator")
 end
 if mods["angelsbioprocessing"] and mods["bobgreenhouse"] then --checks both bio and greenhouse
-	omni.lib.replace_all_ingredient("seedling","omniseedling")
-	omni.lib.replace_recipe_result("wood-sawing-manual","wood","omniwood")
-	omni.lib.remove_recipe_all_techs("bob-greenhouse")
+    omni.lib.replace_all_ingredient("seedling","omniseedling")
+    omni.lib.replace_recipe_result("wood-sawing-manual","wood","omniwood")
+    omni.lib.remove_recipe_all_techs("bob-greenhouse")
 
-	data.raw.recipe["wood-sawing-manual"].icons[1].icon = data.raw.item["omniwood"].icons[1].icon
-	data.raw.recipe["wood-sawing-manual"].icons[1].icon_size = 32
-	data.raw.recipe["wood-sawing-manual"].icons[1].scale = 1
-	data.raw.recipe["wood-sawing-manual"].localised_name = {"item-name.omniwood"}
+    data.raw.recipe["wood-sawing-manual"].icons[1].icon = data.raw.item["omniwood"].icons[1].icon
+    data.raw.recipe["wood-sawing-manual"].icons[1].icon_size = 32
+    data.raw.recipe["wood-sawing-manual"].icons[1].scale = 1
+    data.raw.recipe["wood-sawing-manual"].localised_name = {"item-name.omniwood"}
 end
 --update ingredients
 if mods["bobplates"] then	omni.lib.replace_recipe_ingredient("omnimutator","copper-plate","glass") end
 
 if data.raw.recipe["burner-lab"] then
-	omni.lib.replace_recipe_ingredient("burner-lab","wood","omniwood")
+    omni.lib.replace_recipe_ingredient("burner-lab","wood","omniwood")
 end
