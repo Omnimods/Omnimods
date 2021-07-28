cb328ab1e8ad53c61de95a9ff35b7fb1b7fd28c2
Cereal Sam <62474527+Cereal-Sam@users.noreply.github.com>
2021-07-28 18:46:27 +0200

    Update functions-compatibility.lua

--- omnilib/prototypes/functions/functions-compatibility.lua
+++ omnilib/prototypes/functions/functions-compatibility.lua
@@ -1,5 +1,5 @@
 function omni.lib.angels_component_patch()
-  if mods['angelsindustries'] and angelsmods.industries.components then 
+  if mods['angelsindustries'] and angelsmods.industries.components then
     add_con_mats()
     replace_gen_mats()
     if angelsmods.industries.return_ingredients then
