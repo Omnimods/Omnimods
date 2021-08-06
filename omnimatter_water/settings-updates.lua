73f242c6d8e19651043a28d4a462ac2eb2dce5cc
Cereal Sam <62474527+Cereal-Sam@users.noreply.github.com>
2021-08-06 18:19:41 +0200

    copied comment slipped through...

--- omnimatter_water/settings-updates.lua
+++ omnimatter_water/settings-updates.lua
@@ -1,4 +1,4 @@
---Force tech overhaul to be disabled and hide it until we provide a playable compat
+--Hide waste water setting without angels
 if not mods["angelsrefining"] then
     omni.lib.hide_setting("bool-setting", "enable-waste-water")
 end
