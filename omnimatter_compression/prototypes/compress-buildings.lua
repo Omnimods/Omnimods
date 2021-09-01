19483f3691aa093fec2c8fd92d22f081ada64e34
oorzkws <65210810+oorzkws@users.noreply.github.com>
2021-09-01 02:04:46 -0600

    Fix missing fluid condensation recipes

--- omnimatter_compression/prototypes/compress-buildings.lua
+++ omnimatter_compression/prototypes/compress-buildings.lua
@@ -756,7 +756,7 @@ end
 for fluidname, fluid in pairs(data.raw.fluid) do
     if fluid.fuel_value and not fluidname:find("concentrated%-") then
         for i = 1, omni.compression.bld_lvls do
-            create_concentrated_fluid(fluidname,i)
+            create_concentrated_recipe(fluidname, i)
         end
     end
 end
