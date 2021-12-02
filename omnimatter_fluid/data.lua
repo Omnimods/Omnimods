dbbe0d59169d9ba5318866a8eb47ef84f161163c
Cereal Sam <62474527+Cereal-Sam@users.noreply.github.com>
2021-12-02 13:38:41 +0100

    Moved functions.lua, locale cleanup

--- omnimatter_fluid/data.lua
+++ omnimatter_fluid/data.lua
@@ -4,5 +4,5 @@ if not omni.fluid then omni.fluid={} end
 omni.fluid.sluid_contain_fluid = 40
 omni.fluid.sluid_stack_size = 360
 
-require("functions")
+require("prototypes.functions")
 require("prototypes.sluids-boiler")
