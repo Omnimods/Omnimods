ac173c2c6fa3fa5ee56ac140b2db6683ce8700ae
Cereal Sam <62474527+Cereal-Sam@users.noreply.github.com>
2022-01-03 16:36:14 +0100

    changelog dates

--- omnimatter/settings.lua
+++ omnimatter/settings.lua
@@ -11,8 +11,8 @@ data:extend({
     name = "omnimatter-beginner-multiplier",
     setting_type = "startup",
     default_value = 1,
-    maximum_value = 5,
     minimum_value = 0.20,
+    maximum_value = 5,
     order = "ac"
   },
   {
@@ -20,8 +20,8 @@ data:extend({
     name = "omnimatter-max-tier",
     setting_type = "startup",
     default_value = 5,
-    maximum_value = 8,
     minimum_value = 3,
+    maximum_value = 8,
     allowed_values = {3,4,5,6,7,8},
     order = "ad"
   },
@@ -30,8 +30,8 @@ data:extend({
     name = "omnimatter-pure-lvl-per-tier",
     setting_type = "startup",
     default_value = 2,
-    maximum_value = 6,
     minimum_value = 1,
+    maximum_value = 6,
     allowed_values = {1,2,3,4,5,6},
     order = "ae"
   },
@@ -40,8 +40,8 @@ data:extend({
     name = "omnimatter-impure-lvls",
     setting_type = "startup",
     default_value = 3,
-    maximum_value = 6,
     minimum_value = 2,
+    maximum_value = 6,
     allowed_values = {2,3,4,5,6},
     order = "af"
   },
@@ -50,8 +50,8 @@ data:extend({
     name = "omnimatter-fluid-lvl",
     setting_type = "startup",
     default_value = 6,
-    maximum_value = 10,
     minimum_value = 2,
+    maximum_value = 10,
     allowed_values = {2,3,4,5,6,7,8,9,10},
     order = "ag"
   },
@@ -60,8 +60,8 @@ data:extend({
     name = "omnimatter-fluid-lvl-per-tier",
     setting_type = "startup",
     default_value = 2,
-    maximum_value = 3,
     minimum_value = 1,
+    maximum_value = 3,
     allowed_values = {1,2,3},
     order = "ah"
   },
@@ -70,8 +70,8 @@ data:extend({
     name = "omnimatter-pure-tech-tier-cost-increase",
     setting_type = "startup",
     default_value = 2,
-    maximum_value = 5,
     minimum_value = 1.0,
+    maximum_value = 5,
     order = "ai"
   },
   {
@@ -79,8 +79,8 @@ data:extend({
     name = "omnimatter-pure-tech-level-cost-increase",
     setting_type = "startup",
     default_value = 0.3,
-    maximum_value = 20,
     minimum_value = 0.0001,
+    maximum_value = 20,
     order = "aj"
   },
   {
@@ -95,8 +95,8 @@ data:extend({
     name = "omnimatter-science-pack-constant",
     setting_type = "startup",
     default_value = 2,
-    maximum_value = 10,
     minimum_value = 0.00,
+    maximum_value = 10,
     order = "al"
   },
   {
