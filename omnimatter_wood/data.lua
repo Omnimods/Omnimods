d1d4b963e3304c0c70244f1e46d4827998c568a7
Cereal Sam <62474527+Cereal-Sam@users.noreply.github.com>
2022-01-30 11:55:03 +0100

    Fix omniwood function call fail

--- omnimatter_wood/data.lua
+++ omnimatter_wood/data.lua
@@ -4,4 +4,6 @@ require("prototypes.angels-bioprocessing")
 require("prototypes.bioindustries")
 
 --update electronics if bobs-electronics
-if bobmods and bobmods.electronics then omni.lib.add_recipe_ingredient("omnimutator",{"basic-circuit-board",2}) end
+if bobmods and bobmods.electronics then
+    omni.lib.add_recipe_ingredient("omnimutator",{"basic-circuit-board",2})
+end
