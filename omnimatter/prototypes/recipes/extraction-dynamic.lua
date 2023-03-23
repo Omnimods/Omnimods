dfd2b2b6c83808dd6942a56f3681c5d47ccdd32d
Cereal Sam <62474527+Cereal-Sam@users.noreply.github.com>
2023-03-23 11:36:31 +0100

    compared crude ore names with "normal" ore names

--- omnimatter/prototypes/recipes/extraction-dynamic.lua
+++ omnimatter/prototypes/recipes/extraction-dynamic.lua
@@ -383,7 +383,8 @@ local function create_impure_extraction(tier, split, ore_name)
             focused_ore:setCategory("omnite-extraction")
         end
         if i == 1 then
-            if ores_with_fluid[ore_name] and not omni.matter.omnitial[ore_name] then
+            base_ore_name = ore_name:gsub("crude%-", "")
+            if ores_with_fluid[ore_name] and not omni.matter.omnitial[base_ore_name] then
                 focused_ore:setTechPrereq("omnitech-refinement-"..ore_name)
             elseif tier == 1 then
                 focused_ore:setTechPrereq("omnitech-base-impure-extraction")
