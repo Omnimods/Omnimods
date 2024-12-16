5fa17066c55ffb8a6f659895588056650e5cfc42
Cereal Sam <62474527+Cereal-Sam@users.noreply.github.com>
2024-12-16 20:49:47 +0100

    Fix crashing on burner generators due to changed emission type

--- omnimatter_compression/prototypes/compress-buildings.lua
+++ omnimatter_compression/prototypes/compress-buildings.lua
@@ -513,7 +513,7 @@ if settings.startup["omnicompression_entity_compression"].value then
         --Generators!
         if kind == "burner-generator" then
             new.max_power_output = new_effect(new.max_power_output, compr_lvl)
-            new.burner.emissions_per_minute.pollution = (new.burner.emissions_per_minute.pollution or 0) * math.pow(multiplier,compr_lvl+1)
+            new.burner.emissions_per_second.pollution = (new.burner.emissions_per_second.pollution or 0) * math.pow(multiplier,compr_lvl+1)
         end
 
         --Rockets!
