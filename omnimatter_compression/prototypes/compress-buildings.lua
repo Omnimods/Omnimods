6515d82bbdc691444ee7deb61b92fdcebb3bbd8a
ldinc <drogunov.igor@gmail.com>
2024-12-17 04:42:48 +0300

    [omnimatter_compression] fix optional field per burner generator

--- omnimatter_compression/prototypes/compress-buildings.lua
+++ omnimatter_compression/prototypes/compress-buildings.lua
@@ -514,8 +514,8 @@ if settings.startup["omnicompression_entity_compression"].value then
         if kind == "burner-generator" then
             new.max_power_output = new_effect(new.max_power_output, compr_lvl)
             
-            if new.burner and new.burner.emissions_per_second and new.burner.emissions_per_second.pollution then
-                new.burner.emissions_per_second.pollution = (new.burner.emissions_per_second.pollution or 0) * math.pow(multiplier,compr_lvl+1)
+            if new.burner and new.burner.emissions_per_minute and new.burner.emissions_per_minute.pollution then
+                new.burner.emissions_per_minute.pollution = (new.burner.emissions_per_minute.pollution or 0) * math.pow(multiplier,compr_lvl+1)
             end
         end
 
