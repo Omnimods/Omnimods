8f28c8cce098c8f6a3b33fca1d2f31692577dd76
Adam Cirillo <adamncirillo@gmail.com>
2026-01-06 06:00:24 +1100

    combied loaders into one lua and small fixes

--- omnimatter_energy/prototypes/compat/loaders-modernized.lua
+++
@@ -1,6 +0,0 @@
---Loaders Modernized compatibility
---(The Chute recipe is based on basic belt ingredients (mostly just iron), so we need to add omnitors to that recipe aswell)
-if data.raw.recipe["chute-mdrn-loader"] then
-    omni.lib.add_recipe_ingredient("chute-mdrn-loader",{type = "item", name ="omnitor", amount = 2})
-    omni.lib.add_science_pack("mdrn-loader","logistic-science-pack")
-end
