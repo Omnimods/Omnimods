98f7a5b12fc5d74da53a7ce6fcc7a06b92b8ad0d
Adam Cirillo <adamncirillo@gmail.com>
2026-01-25 19:23:48 +1100

    Fixed compact with loaders with Bob's logistics

--- omnimatter_energy/prototypes/compat/loaders.lua
+++ omnimatter_energy/prototypes/compat/loaders.lua
@@ -1,15 +1,22 @@
 --Loaders Modernized compatibility
 --(The Chute recipe is based on basic belt ingredients (mostly just iron), so we need to add omnitors to that recipe aswell)
 if data.raw.recipe["chute-mdrn-loader"] then
-    omni.lib.add_recipe_ingredient("chute-mdrn-loader",{type = "item", name ="omnitor", amount = 2})
-    omni.lib.add_science_pack("mdrn-loader","logistic-science-pack")
+    omni.lib.add_recipe_ingredient("chute-mdrn-loader", { type = "item", name = "omnitor", amount = 2 })
+    omni.lib.add_science_pack("mdrn-loader", "logistic-science-pack")
+    if data.raw.recipe["bob-basic-underground-belt"] then
+        omni.lib.replace_recipe_ingredient("basic-mdrn-loader", "bob-basic-underground-belt", "basic-underground-belt")
+    end
 end
 
 --Miniloader Redux compatibility
---(The Chute recipe is based on basic belt ingredients (mostly just iron), so we need to add omnitors to that recipe aswell)
+--(The Chute recipe uses transport belts replace with basic transport belts)
 if data.raw.recipe["chute-mdrn-loader"] then
     --omni.lib.add_recipe_ingredient("hps__ml-chute-miniloader",{type = "item", name ="omnitor", amount = 2})
     omni.lib.replace_recipe_ingredient("hps__ml-chute-miniloader", "transport-belt", "basic-transport-belt")
     --omni.lib.replace_recipe_ingredient("burner-inserter","omnitor","kr-inserter-parts")
-    omni.lib.add_science_pack("hps__ml-miniloader","logistic-science-pack")
+    omni.lib.add_science_pack("hps__ml-miniloader", "logistic-science-pack")
+    if data.raw.recipe["bob-basic-underground-belt"] then
+        omni.lib.replace_recipe_ingredient("hps__ml-bob-basic-miniloader", "bob-basic-underground-belt",
+            "basic-underground-belt")
+    end
 end
