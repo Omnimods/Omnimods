7952ce680e746ab2e5b00d6e651a6fe6d373cea5
Adam Cirillo <adamncirillo@gmail.com>
2026-01-29 06:33:17 +1100

    Updated Bio Industries 2 Compact

--- omnimatter_science/prototypes/updates.lua
+++ omnimatter_science/prototypes/updates.lua
@@ -1,7 +1,7 @@
 if mods["angelsbioprocessing"] then
-    omni.lib.add_unlock_recipe("angels-bio-farm","angels-temperate-garden-cultivating-b")
-    omni.lib.add_unlock_recipe("angels-bio-farm","angels-desert-garden-cultivating-b")
-    omni.lib.add_unlock_recipe("angels-bio-farm","angels-swamp-garden-cultivating-b")
+    omni.lib.add_unlock_recipe("angels-bio-farm", "angels-temperate-garden-cultivating-b")
+    omni.lib.add_unlock_recipe("angels-bio-farm", "angels-desert-garden-cultivating-b")
+    omni.lib.add_unlock_recipe("angels-bio-farm", "angels-swamp-garden-cultivating-b")
 end
 
 --Add Omni science pack if crystal is present
@@ -9,7 +9,7 @@ if mods["omnimatter_crystal"] then
     --UPDATE LABS INPUT
     for i, labs in pairs(data.raw["lab"]) do
         local found = false
-        for _,v in pairs(labs.inputs) do
+        for _, v in pairs(labs.inputs) do
             if v == "omni-pack" then
                 found = true
             end
@@ -20,12 +20,12 @@ if mods["omnimatter_crystal"] then
     end
 
     --SP ingredient manipulation
-    omni.lib.add_recipe_ingredient("chemical-science-pack","basic-crystallonic")
-    omni.lib.add_recipe_ingredient("production-science-pack","basic-oscillo-crystallonic")
-    omni.lib.add_recipe_ingredient("utility-science-pack","basic-oscillo-crystallonic")
+    omni.lib.add_recipe_ingredient("chemical-science-pack", "basic-crystallonic")
+    omni.lib.add_recipe_ingredient("production-science-pack", "basic-oscillo-crystallonic")
+    omni.lib.add_recipe_ingredient("utility-science-pack", "basic-oscillo-crystallonic")
 
     --SP tech manipulation
-    omni.lib.remove_science_pack("omnitech-crystallonics-4","utility-science-pack")
+    omni.lib.remove_science_pack("omnitech-crystallonics-4", "utility-science-pack")
     omni.lib.add_science_pack("electric-engine", "omni-pack")
 
     if not mods["pycoalprocessing"] then
@@ -47,13 +47,13 @@ if mods["omnimatter_crystal"] then
     --Omni
     if not mods["omnimatter_research"] then
         if mods["boblogistics"] and settings.startup["bobmods-logistics-inserteroverhaul"].value == true then
-            omni.lib.add_recipe_ingredient("omni-pack",{type = "item", name = "long-handed-inserter", amount = 1})
+            omni.lib.add_recipe_ingredient("omni-pack", { type = "item", name = "long-handed-inserter", amount = 1 })
         else
-            omni.lib.add_recipe_ingredient("omni-pack",{type = "item", name = "fast-inserter", amount = 1})
+            omni.lib.add_recipe_ingredient("omni-pack", { type = "item", name = "fast-inserter", amount = 1 })
         end
     end
 
-    if mods["omnimatter_wood"] then 
+    if mods["omnimatter_wood"] then
         omni.lib.replace_science_pack("omnitech-omnimutator-2", "chemical-science-pack", "omni-pack")
     end
 
@@ -67,7 +67,7 @@ if mods["omnimatter_crystal"] then
         omni.lib.replace_science_pack("angels-ore-leaching", "chemical-science-pack", "omni-pack")
         omni.lib.replace_science_pack("angels-ore-processing-2", "chemical-science-pack", "omni-pack")
         omni.lib.replace_science_pack("angels-ore-processing-3", "production-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-ore-processing-4","utility-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-ore-processing-4", "utility-science-pack", "omni-pack")
         omni.lib.replace_science_pack("angels-ore-leaching", "chemical-science-pack", "omni-pack")
         omni.lib.replace_science_pack("angels-slag-processing-2", "chemical-science-pack", "omni-pack")
         omni.lib.replace_science_pack("angels-thermal-water-extraction-2", "chemical-science-pack", "omni-pack")
@@ -79,8 +79,8 @@ if mods["omnimatter_crystal"] then
     end
 
     if mods["angelspetrochem"] then
-        omni.lib.replace_science_pack("angels-advanced-gas-processing","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-advanced-oil-processing","chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-advanced-gas-processing", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-advanced-oil-processing", "chemical-science-pack", "omni-pack")
         omni.lib.add_science_pack("angels-advanced-chemistry-2", "omni-pack")
         omni.lib.add_science_pack("angels-gas-steam-cracking-1", "omni-pack")
         omni.lib.add_science_pack("angels-oil-steam-cracking-1", "omni-pack")
@@ -89,44 +89,44 @@ if mods["omnimatter_crystal"] then
     end
 
     if mods["angelssmelting"] then
-        omni.lib.replace_science_pack("angels-aluminium-smelting-2","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-bronze-smelting-3","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-copper-smelting-3","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-iron-smelting-3","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-nitinol-smelting-2","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-steel-smelting-2","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-tin-smelting-3","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-solder-smelting-3","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-lead-smelting-3","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-silver-smelting-2","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-zinc-smelting-2","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-chrome-smelting-2","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-cobalt-smelting-2","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-manganese-smelting-2","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-nickel-smelting-2","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-silicon-smelting-2","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-titanium-smelting-2","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-tungsten-smelting-2","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-platinum-smelting-2","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-glass-smelting-3","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-stone-smelting-3","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-metallurgy-3","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-metallurgy-4","utility-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-powder-metallurgy-2","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-strand-casting-2","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-thermal-water-extraction","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-ore-advanced-floatation","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("omnitech-angels-omnicium-smelting-3","chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-aluminium-smelting-2", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-bronze-smelting-3", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-copper-smelting-3", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-iron-smelting-3", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-nitinol-smelting-2", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-steel-smelting-2", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-tin-smelting-3", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-solder-smelting-3", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-lead-smelting-3", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-silver-smelting-2", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-zinc-smelting-2", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-chrome-smelting-2", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-cobalt-smelting-2", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-manganese-smelting-2", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-nickel-smelting-2", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-silicon-smelting-2", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-titanium-smelting-2", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-tungsten-smelting-2", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-platinum-smelting-2", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-glass-smelting-3", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-stone-smelting-3", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-metallurgy-3", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-metallurgy-4", "utility-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-powder-metallurgy-2", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-strand-casting-2", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-thermal-water-extraction", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-ore-advanced-floatation", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("omnitech-angels-omnicium-smelting-3", "chemical-science-pack", "omni-pack")
         omni.lib.add_science_pack("angels-coolant-1", "omni-pack")
 
-        if mods ["Clowns-Extended-Minerals"] then
-            omni.lib.replace_science_pack("clowns-ore-leaching","chemical-science-pack", "omni-pack")
+        if mods["Clowns-Extended-Minerals"] then
+            omni.lib.replace_science_pack("clowns-ore-leaching", "chemical-science-pack", "omni-pack")
         end
     end
 
     if mods["angelsbioprocessing"] then
-        omni.lib.replace_science_pack("angels-bio-paper-3","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("angels-bio-processing-2","chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-bio-paper-3", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("angels-bio-processing-2", "chemical-science-pack", "omni-pack")
         omni.lib.add_science_pack("angels-bio-refugium-fish-1", "omni-pack")
         omni.lib.add_science_pack("angels-bio-processing-paste", "omni-pack")
     end
@@ -145,13 +145,13 @@ if mods["omnimatter_crystal"] then
     end
 
     --Misc
-    if mods["Bio_Industries"] then
-        omni.lib.replace_science_pack("bi-advanced-biotechnology","chemical-science-pack", "omni-pack")
-        omni.lib.replace_science_pack("bi-organic-plastic","production-science-pack", "omni-pack")
+    if mods["Bio_Industries_2"] then
+        omni.lib.replace_science_pack("bi-tech-advanced-biotechnology", "chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("bi-tech-organic-plastic", "production-science-pack", "omni-pack")
     end
 
     if mods["Factorissimo2"] then
-        omni.lib.replace_science_pack("factory-architecture-t1","chemical-science-pack", "omni-pack")
+        omni.lib.replace_science_pack("factory-architecture-t1", "chemical-science-pack", "omni-pack")
     end
 
     --Add Omni SP to all techs that have manually added techs as prereq
--- omnimatter_wood/prototypes/bioindustries.lua
+++ omnimatter_wood/prototypes/bioindustries.lua
@@ -1,7 +1,6 @@
-if mods["Bio_Industries"] then
-
+if mods["Bio_Industries_2"] then
     if not mods["bobgreenhouse"] then
-        ItemGen:create("omnimatter_wood","omniseedling"):
+        ItemGen:create("omnimatter_wood", "omniseedling"):
             setFuelValue(0.7):
             setSubgroup("omnimutator-items"):
             setStacksize(100):extend()
@@ -11,85 +10,101 @@ if mods["Bio_Industries"] then
     omni.lib.replace_all_ingredient("seedling", "omniseedling")
     omni.lib.replace_all_result("seedling", "omniseedling")
 
-    data.raw.recipe["bi-seedling-1"].category="omnimutator"
-    data.raw.recipe["bi-seedling-1"].icon=nil
-    data.raw.recipe["bi-seedling-1"].icons={
-        {icon="__omnimatter_wood__/graphics/icons/Seedling.png", icon_size=32},
-        {icon="__base__/graphics/icons/fluid/water.png",
-        scale = 0.23,
-        shift = { 10, 10},
-        icon_size = 64}
+    data.raw.recipe["bi-seedling-1"].category = "omnimutator"
+    data.raw.recipe["bi-seedling-1"].icon = nil
+    data.raw.recipe["bi-seedling-1"].icons = {
+        { icon = "__omnimatter_wood__/graphics/icons/Seedling.png", icon_size = 32 },
+        {
+            icon = "__base__/graphics/icons/fluid/water.png",
+            scale = 0.23,
+            shift = { 10, 10 },
+            icon_size = 64
+        }
     }
 
-    data.raw.recipe["bi-seedling-2"].category="omnimutator"
-    data.raw.recipe["bi-seedling-2"].icon=nil
-    data.raw.recipe["bi-seedling-2"].icons={
-        {icon="__omnimatter_wood__/graphics/icons/Seedling.png", icon_size=32},
-        {icon="__Bio_Industries__/graphics/icons/ash_64.png",
-        scale = 0.4375,
-        shift = { 10, 10},
-        icon_size=64,}
+    data.raw.recipe["bi-seedling-2"].category = "omnimutator"
+    data.raw.recipe["bi-seedling-2"].icon = nil
+    data.raw.recipe["bi-seedling-2"].icons = {
+        { icon = "__omnimatter_wood__/graphics/icons/Seedling.png", icon_size = 32 },
+        {
+            icon = "__Bio_Industries_2__/graphics/icons/ash.png",
+            scale = 0.4375,
+            shift = { 10, 10 },
+            icon_size = 64,
+        }
     }
 
-    data.raw.recipe["bi-seedling-3"].category="omnimutator"
-    data.raw.recipe["bi-seedling-3"].icon=nil
-    data.raw.recipe["bi-seedling-3"].icons={
-        {icon="__omnimatter_wood__/graphics/icons/Seedling.png", icon_size=32},
-        {icon="__Bio_Industries__/graphics/icons/fertilizer_64.png",
-        scale = 0.4375,
-        shift = { 10, 10},
-        icon_size=64,}
+    data.raw.recipe["bi-seedling-3"].category = "omnimutator"
+    data.raw.recipe["bi-seedling-3"].icon = nil
+    data.raw.recipe["bi-seedling-3"].icons = {
+        { icon = "__omnimatter_wood__/graphics/icons/Seedling.png", icon_size = 32 },
+        {
+            icon = "__Bio_Industries_2__/graphics/icons/fertilizer.png",
+            scale = 0.4375,
+            shift = { 10, 10 },
+            icon_size = 64,
+        }
     }
 
-    data.raw.recipe["bi-seedling-4"].category="omnimutator"
-    data.raw.recipe["bi-seedling-4"].icon=nil
-    data.raw.recipe["bi-seedling-4"].icons={
-        {icon="__omnimatter_wood__/graphics/icons/Seedling.png", icon_size=32},
-        {icon="__Bio_Industries__/graphics/icons/advanced_fertilizer_64.png",
-        scale = 0.4375,
-        shift = { 10, 10},
-        icon_size=64,}
+    data.raw.recipe["bi-seedling-4"].category = "omnimutator"
+    data.raw.recipe["bi-seedling-4"].icon = nil
+    data.raw.recipe["bi-seedling-4"].icons = {
+        { icon = "__omnimatter_wood__/graphics/icons/Seedling.png", icon_size = 32 },
+        {
+            icon = "__Bio_Industries_2__/graphics/icons/fertilizer_advanced.png",
+            scale = 0.4375,
+            shift = { 10, 10 },
+            icon_size = 64,
+        }
     }
 
 
-    omni.lib.replace_recipe_result("bi-logs-1","wood",{name = "omniwood", amount =60})
-    data.raw.recipe["bi-logs-1"].icon=nil
-    data.raw.recipe["bi-logs-1"].icons={
-        {icon="__omnimatter_wood__/graphics/icons/mutated-wood2.png", icon_size=32},
-        {icon="__base__/graphics/icons/fluid/water.png",
-        scale = 0.23,
-        shift = { 10, 10},
-        icon_size = 64}
+    omni.lib.replace_recipe_result("bi-logs-1", "wood", { name = "omniwood", amount = 60 })
+    data.raw.recipe["bi-logs-1"].icon = nil
+    data.raw.recipe["bi-logs-1"].icons = {
+        { icon = "__omnimatter_wood__/graphics/icons/mutated-wood2.png", icon_size = 32 },
+        {
+            icon = "__base__/graphics/icons/fluid/water.png",
+            scale = 0.23,
+            shift = { 10, 10 },
+            icon_size = 64
+        }
     }
 
-    omni.lib.replace_recipe_result("bi-logs-2","wood",{name = "omniwood", amount =100})
-    data.raw.recipe["bi-logs-2"].icon=nil
-    data.raw.recipe["bi-logs-2"].icons={
-        {icon="__omnimatter_wood__/graphics/icons/mutated-wood2.png", icon_size=32},
-        {icon="__Bio_Industries__/graphics/icons/ash_64.png",
-        scale = 0.4375,
-        shift = { 10, 10},
-        icon_size=64,}
+    omni.lib.replace_recipe_result("bi-logs-2", "wood", { name = "omniwood", amount = 100 })
+    data.raw.recipe["bi-logs-2"].icon = nil
+    data.raw.recipe["bi-logs-2"].icons = {
+        { icon = "__omnimatter_wood__/graphics/icons/mutated-wood2.png", icon_size = 32 },
+        {
+            icon = "__Bio_Industries_2__/graphics/icons/ash.png",
+            scale = 0.4375,
+            shift = { 10, 10 },
+            icon_size = 64,
+        }
     }
 
-    omni.lib.replace_recipe_result("bi-logs-3","wood",{name = "omniwood", amount =150})
-    data.raw.recipe["bi-logs-3"].icon=nil
-    data.raw.recipe["bi-logs-3"].icons={
-        {icon="__omnimatter_wood__/graphics/icons/mutated-wood2.png", icon_size=32},
-        {icon="__Bio_Industries__/graphics/icons/fertilizer_64.png",
-        scale = 0.4375,
-        shift = { 10, 10},
-        icon_size=64,}
+    omni.lib.replace_recipe_result("bi-logs-3", "wood", { name = "omniwood", amount = 150 })
+    data.raw.recipe["bi-logs-3"].icon = nil
+    data.raw.recipe["bi-logs-3"].icons = {
+        { icon = "__omnimatter_wood__/graphics/icons/mutated-wood2.png", icon_size = 32 },
+        {
+            icon = "__Bio_Industries_2__/graphics/icons/fertilizer.png",
+            scale = 0.4375,
+            shift = { 10, 10 },
+            icon_size = 64,
+        }
     }
 
-    omni.lib.replace_recipe_result("bi-logs-4","wood",{name = "omniwood", amount =250})
-    data.raw.recipe["bi-logs-4"].icon=nil
-    data.raw.recipe["bi-logs-4"].icons={
-        {icon="__omnimatter_wood__/graphics/icons/mutated-wood2.png", icon_size=32},
-        {icon="__Bio_Industries__/graphics/icons/advanced_fertilizer_64.png",
-        scale = 0.4375,
-        shift = { 10, 10},
-        icon_size=64,}
+    omni.lib.replace_recipe_result("bi-logs-4", "wood", { name = "omniwood", amount = 250 })
+    data.raw.recipe["bi-logs-4"].icon = nil
+    data.raw.recipe["bi-logs-4"].icons = {
+        { icon = "__omnimatter_wood__/graphics/icons/mutated-wood2.png", icon_size = 32 },
+        {
+            icon = "__Bio_Industries_2__/graphics/icons/fertilizer_advanced.png",
+            scale = 0.4375,
+            shift = { 10, 10 },
+            icon_size = 64,
+        }
     }
 
     omni.lib.add_prerequisite("bi-tech-bio-farming", "omnitech-omnimutator")
